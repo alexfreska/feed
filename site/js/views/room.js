f4ca1bc0c1ae3e9214cd5da56e4979b1039ec5d0
alexfreska <alex.freska@gmail.com>
2014-06-02 18:08:45 -0500

    minor diff

--- site/js/views/room.js
+++ site/js/views/room.js
@@ -14,17 +14,17 @@ define(function(require) {
         tagName: 'div',
         className: 'room',
         template: _.template(RoomT),
-        
+
         events: {
             'click .submit': 'submit'
         },
-        
+
         initialize: function (args) {
             var s = this;
-    
+
             // using simple array as the collection for now
-            s.posts = [];   
-            
+            s.posts = [];
+
             // hash for tracking feed views
             s.feeds = [];
 
@@ -48,24 +48,24 @@ define(function(require) {
             s.$el.attr('id',s.room);
 
             s.$el.append(this.template());
-            
+
             // create initial feeds
             s.mainFeed = s.addFeed('main',1);
-            s.linkFeed = s.addFeed('links'); 
-            s.codeFeed = s.addFeed('code'); 
+            s.linkFeed = s.addFeed('links');
+            s.codeFeed = s.addFeed('code');
 
             // add main feed and add a post form
             //s.$('.feeds').prepend( s.mainFeed.el );
             s.mainFeed.$el.append(_.template(PostFormT)());
-            
+
             //add event listener
             window.socket.on(s.room, function (data) {
 
                 // console.log('recieved data: ');
                 // console.log(data);
-                
+
                 if(data.type == 'init') {
-                    
+
                     _.each(data.posts.reverse(), function(message,index) {
                         if(index == data.posts.length-1) {
                             s.process(message,{noScroll: 0});
@@ -105,7 +105,7 @@ define(function(require) {
 
             //collect data
             var data = s.$('.input').val();
-            
+
             // strip whitespace with sanitize function
             data = s.sanitize(data);
 
@@ -140,34 +140,34 @@ define(function(require) {
         },
         process: function (message,options) {
             var s = this;
-    
+
             if(message.type == 'content') {
-    
-                s.addPost(message,options); 
+
+                s.addPost(message,options);
             }
             else if(message.type == 'action') {
-    
+
                 s.action(message);
-    
+
             }
-    
+
         },
         // Adds an incoming post to the rooms feeds
         addPost: function (message,options) {
             var s = this;
-            
+
             //make a model
             var post = new Post(message);
-            
+
             // Save model in posts array by _id
             s.posts[message._id] = post;
 
             //render and append post
             s.mainFeed.post(post,options);
-    
+
             // check for links
             var links = message.text.match(/((https?:\/\/)?([\da-z-]+[\.])+([a-z]{2,4})(\/[\w\da-z\-^\/\.]*)+\/?)/g)? 1 : 0;
-            
+
             if(links) {
                 s.linkFeed.post(post,options);
             }
@@ -180,25 +180,25 @@ define(function(require) {
             } else {
                 code = 0;
             }
-            
+
             if(code) {
                 message.text = message.text.replace(/```/,'<pre><code>');
                 message.text = message.text.replace(/```/,'</code></pre>');
-            
+
                 post.set('text', message.text);
 
                 code = s.codeFeed.post(post,options);
-                
+
                 code.$('.content pre code').each(function() {
                         hljs.highlightBlock(this, '    ');
                         //hljs.highlightAuto(this.textContent);
                 });
 
             }
-    
+
             // check for hash tags
             var tags = message.text.match(/#([\S]+)/g);
-            
+
             _.each(tags, function (tag) {
                 tag = tag.substr(1, tag.length-1);
                 if(s.selectors[tag]) {
@@ -216,31 +216,31 @@ define(function(require) {
         },
         action: function (message) {
             var s = this;
-  
+
             // a user has joined the room
             if(message.action == 'join') {
-                
+
                 console.log('join');
-                
+
                 console.log(message.text);
                 // ERROR: sometimes its null
                 if(message.text && message.text.user) {
-                    // add user to user list    
+                    // add user to user list
                     s.$('.userList').append(_.template(UserT)({data: message.text.user}));
                 }
 
-            } 
-            // a user has left the room 
+            }
+            // a user has left the room
             else if(message.action == 'leave') {
 
                 // find and remove user from list
                 var selector = '.userList #'+message.text.user.name+message.text.user.hash;
                 s.$(selector).remove();
 
-            } 
+            }
             // the action is related to a post
             else {
-                
+
                 // the text of an action message is the relevant post's _id
                 // grab relevant post
                 var post = s.posts[message.text];
@@ -248,7 +248,7 @@ define(function(require) {
                 if(post) {
                     if(message.action == 'delete') {
 
-                        // set deleted, this will fire model change and the post 
+                        // set deleted, this will fire model change and the post
                         // will re-render into a deleted content state
                         // also clear out the text
                         post.set({'deleted': 1, 'text': ''});
@@ -258,14 +258,14 @@ define(function(require) {
                         // addToReceipt handles receipt updates
                         s.addToReceipt(message);
                     }
-                    
+
                 }
             }
         },
         // this function adds a new feed (for a hashtag)
         addFeed: function (name,primary) {
             var s = this;
-         
+
             var feed = new Feed({name: name});
 
             if(primary) {
@@ -295,12 +295,12 @@ define(function(require) {
             function trim(str, chars) {
                     return ltrim(rtrim(str, chars), chars);
             }
-             
+
             function ltrim(str, chars) {
                     chars = chars || "\\s";
                         return str.replace(new RegExp("^[" + chars + "]+", "g"), "");
             }
-             
+
             function rtrim(str, chars) {
                     chars = chars || "\\s";
                         return str.replace(new RegExp("[" + chars + "]+$", "g"), "");
@@ -311,11 +311,11 @@ define(function(require) {
         addUserToReceipt: function () {
             var s = this;
 
-            if( (s.$el.css('display') != 'none') && 
+            if( (s.$el.css('display') != 'none') &&
                  s.focused &&
-                (s.lastMessageSeen != s.lastMessage._id) && 
+                (s.lastMessageSeen != s.lastMessage._id) &&
                 (s.user.name != s.lastMessage.username) ) {
-               
+
                 // build message block
                 var message = {
                     type: 'action',
@@ -328,14 +328,14 @@ define(function(require) {
 
                 // set out message to roomates
                 window.socket.emit(s.room,message);
-                
+
                 s.lastMessageSeen = s.lastMessage._id;
             }
 
         },
         addToReceipt: function (message) {
             var s = this;
-          
+
             // if the receipt is for the current last message and
             // the message is not from self
             if((message.text == s.lastMessage._id) && (message.email != s.user.email)) {
@@ -352,11 +352,11 @@ define(function(require) {
                     if(s.emptyReceipt) {
 
                         // add the user to receipt
-                        s.mainFeed.$('.receipt').text('Seen by ' + message.username); 
-                       
+                        s.mainFeed.$('.receipt').text('Seen by ' + message.username);
+
                         // receipt is no longer empty
                         s.emptyReceipt = 0;
-                        
+
                     } else {
 
                         // if the receipt is not empty append the user
@@ -367,10 +367,10 @@ define(function(require) {
 
                 }
 
-                // if the incoming receipts text which contains the taget messages id 
+                // if the incoming receipts text which contains the taget messages id
                 // is not equal to the currently recorded last message then reset receipt
             } else if(message.text != s.lastMessage._id) {
-                
+
                 // clear the receipt
                 s.clearReceipt();
             }
