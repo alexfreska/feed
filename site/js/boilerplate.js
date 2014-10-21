ccdb1bb81c7b5f0824264ed718e2000d89a6cfaa
alexfreska <alex.freska@gmail.com>
2014-10-20 20:07:14 -0400

    blank branch

--- site/js/boilerplate.js
+++
@@ -1,8 +0,0 @@
-define(function(require) {
-    var $           = require('jquery'),
-        _           = require('underscore'),
-        Backbone    = require('backbone');
-
-    return {};
-
-});
